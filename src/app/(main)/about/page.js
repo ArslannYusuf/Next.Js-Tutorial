c394c4cf7e0a1837c892510c2da6758b38b270e2
Yusuf Arslan <yeed060118@gmail.com>
2024-07-02 21:31:27 +0300

    images

--- src/app/(main)/about/page.js
+++ src/app/(main)/about/page.js
@@ -1,19 +1,21 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-	return (
-		<div>
-			<h1>About Page</h1>
-			
+  return (
+    <div>
+      <h1>About Page</h1>
 
-			{[...new Array(100)].map((_, index) => (
-				<p key={index}>.</p>
-			))}
+      <Image src="/images/iwatch.png" width={200} height={200} />
 
-            <Link href="/dashboard/products">Dashboard</Link>
-		</div>
-	);
+      {[...new Array(100)].map((_, index) => (
+        <p key={index}>.</p>
+      ))}
+
+      <Link href="/dashboard/products">Dashboard</Link>
+    </div>
+  );
 };
 
 export default Page;
