d66d2528d4bda30dbf78e1c88180addaa4adfe53
Yusuf Arslan <yeed060118@gmail.com>
2024-07-03 20:49:11 +0300

    remote image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+/* dis kaynaklardan alinan image'ler burada bulunmaktadir */
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.unsplash.com',
+          },
+        ],
+    }
+};
 
 export default nextConfig;
